ba49339b55bc859e12b1ab62b4b83636af88b29c
anhphung75 <anhphung75@gmail.com>
2020-05-28 17:42:47 +0700

    tiep 20200528

--- services/webapp/static/js/hoso/app.js
+++ services/webapp/static/js/hoso/app.js
@@ -1,54 +1,45 @@
-var tblKhachhang = {
-  name: 'Khachhang',
-  columns: {
-    // Here "Id" is name of column 
-    autoid: { primaryKey: true, autoIncrement: true },
-    makhachhang: { notNull: true, dataType: "string", default: "2020KH" },
-    hoten: { notNull: true, dataType: "string" },
-    diachi: { notNull: true, dataType: "string" },
-    lienhe: { notNull: false, dataType: "string" },
-    map: { notNull: false, dataType: "string" },
-    maqp: { notNull: false, dataType: "string" },
-    lastupdate: { notNull: true, dataType: "date_time", default: new Date() }
-  }
-};
-
-var tblHoso = {
-  name: 'Hoso',
-  columns: {
-    // Here "Id" is name of column 
-    autoid: { primaryKey: true, autoIncrement: true },
-    mahoso: { notNull: true, dataType: "string" },
-    makhachhang: { notNull: true, dataType: "string" },
-    itemName: { notNull: true, dataType: "string" },
-    price: { notNull: true, dataType: "number" },
-    quantity: { notNull: true, dataType: "number" }
-  }
-};
-
-
 var oDb = {
-  load: async (dbName = '') => {
+  load: async (dbName = '', dbVersion = 1) => {
     let check = dbName.length || 0;
     if (check == 0) { return };
-    let csdl = {
-      name: dbName,
-      tables: [tblKhachhang, tblHoso]
-    };
-    try {
-      return await this.db.initDb(csdl);
-    } catch (error) {
-      //console.error(error);
-    };
+    return new Promise((resolve, reject) => {
+      let request = window.indexedDB.open(dbName, dbVersion);
+      request.onerror = e => {
+        console.log('Error opening db', e);
+        reject('Error');
+      };
+      request.onsuccess = e => {
+        resolve(e.target.result);
+      };
+      //tao bang neu chua co
+      request.onupgradeneeded = e => {
+        console.log('onupgradeneeded');
+        let db = e.target.result;
+        db.createObjectStore('khachhang', { autoIncrement: false, keyPath: 'makhachhang' });
+        db.createObjectStore('hoso', { autoIncrement: false, keyPath: 'mahoso' });
+        resolve(db);
+      };
+    });
   },
   drop: async (dbName = '') => {
     let check = dbName.length || 0;
     if (check == 0) { return };
-    try {
-      return await indexedDB.deleteDatabase(dbName);
-    } catch (error) {
-      // console.error(error);
-    };
+    return new Promise((resolve, reject) => {
+      let request = window.indexedDB.deleteDatabase(dbName);
+      request.onerror = e => {
+        console.log('Error opening db', e);
+        reject('Error');
+      };
+      request.onsuccess = e => {
+        resolve(null);
+      };
+      //tao bang neu chua co
+      request.onupgradeneeded = e => {
+        console.log('onupgradeneeded');
+        window.indexedDB.deleteDatabase(dbName);
+        resolve(null);
+      };
+    });
   }
 };
 
@@ -99,23 +90,129 @@ var app = new Vue({
   delimiters: ["{`", "`}"],
   data() {
     return {
-      db: new JsStore.Connection(),
-      userid: 'Khach',
+      dbName: 'Khach',
+      dbVersion: 1,
+      db: null,
       namlv: 2020,
       showMenu: false,
+      oHsKh: {
+        '2020.hs.001': {
+          mahoso: '2020.hs.001',
+          makhachhang: '2020.kh.001',
+          hoten: 'Tran Van Anh 1',
+          diachi: '123- To Ngoc Van- Q.Td',
+          madot: '2020GMMP001',
+        },
+        '2020.hs.002': {
+          mahoso: '2020.hs.002',
+          makhachhang: '2020.kh.002',
+          hoten: 'Tran Van Anh 2',
+          diachi: '124- To Ngoc Van- Q.Td',
+          madot: '2020GMMP001',
+        },
+        '2020.hs.003': {
+          mahoso: '2020.hs.003',
+          makhachhang: '2020.kh.003',
+          hoten: 'Tran Van Anh 3',
+          diachi: '125- To Ngoc Van- Q.Td',
+          madot: '2020GMMP001',
+        },
+      },
     }
   },
+  async created() {
+    await this.loadDb();
+    //this.cats = await this.getCatsFromDb();
+    //this.ready = true;
+  },
+  async mounted() {
+    // await this.odbHoso();
+  },
   methods: {
-    loaddb() {
-      oDb.load(this.userid);
-      this.moi();
-    },
-    moi() {
-      let oHosomoi = {
-        mahoso: '2020hs001',
-        makhachhang: 'kh001'
+    async loadDb() {
+      let check = this.dbName.length || 0;
+      if (check == 0) { return false; };
+      let request = await window.indexedDB.open(this.dbName, this.dbVersion);
+      request.onerror = e => {
+        console.log('Error opening db', e);
       };
-      oHoso.moi(oHosomoi);
+      request.onsuccess = e => {
+        this.db = e.target.result;
+      };
+      request.onupgradeneeded = e => {
+        this.db = e.target.result;
+        this.db.createObjectStore('khachhang', { autoIncrement: false, keyPath: 'makhachhang' });
+        this.db.createObjectStore('hoso', { autoIncrement: false, keyPath: 'mahoso' });
+      };
+    },
+    async dropDb() {
+      let check = this.dbName.length || 0;
+      if (check == 0) { return false; };
+      let request = await window.indexedDB.deleteDatabase(this.dbName);
+      request.onerror = e => {
+        console.log('Error opening db', e);
+      };
+      await window.location.reload();
+    },
+    odbHoso() {
+      let oRecs = {};
+      let storename = 'hoso';
+      let trans = this.db.transaction([storename,], 'readonly');
+      let store = trans.objectStore(storename);
+      let request = store.openCursor();
+      request.onsuccess = e => {
+        let cursor = e.target.result;
+        if (cursor) {
+          console.log('odbHoso cursor.key=', cursor.key);
+          console.log('odbHoso cursor.value=', cursor.value);
+          let k = cursor.key.toString();
+          oRecs[k] = cursor.value;
+          cursor.continue();
+        };
+      };
+      console.log('odbHoso oRec=', 'toi day ne', JSON.stringify(oRecs));
+      return oRecs;
+    },
+    async suaHoso() {
+      let oRecs = this.oHoso;
+      let storename = 'hoso';
+      console.log('suaHoso oRec=', oRecs);
+      let trans = this.db.transaction([storename,], 'readwrite');
+      let store = trans.objectStore(storename);
+      try {
+        for (const k in oRecs) {
+          let v = oRecs[k];
+          console.log('suaHoso oRec.k=', k);
+          console.log('suaHoso oRec.v=', v);
+          await store.put(v);
+        }
+        // await trans.complete;
+        await this.odbHoso();
+      } catch (err) {
+        console.log('error', err.message);
+      };
+
+
+    },
+  },
+  computed: {
+    async odbKhachhang() {
+      let oRecs = {};
+      let storename = 'khachhang';
+      let trans = this.db.transaction([storename,], 'readonly');
+      let store = trans.objectStore(storename);
+      let request = await store.openCursor();
+      request.onsuccess = e => {
+        let cursor = e.target.result;
+        if (cursor) {
+          oRecs[cursor.key] = cursor.value;
+          cursor.continue();
+        };
+      };
+      return oRecs;
+    },
+    oHoso() {
+      return this.oHsKh;
     }
-  }
+  },
 });
