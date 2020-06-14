929d178f2276197e5f56fbab9d107ea9293b816b
anhphung75 <anhphung75@gmail.com>
2020-06-14 17:40:58 +0700

    tiep 20200614

---
+++ services/webapp/static/js/hoso/ndbang1.js
@@ -0,0 +1,48 @@
+Vue.component('nd-bang1', {
+  props: ['rec'],
+  template: `
+  <div v-if="isedit" class="row m-0 p-0 bg-warning">
+    <div class="col-1 crud text-break">
+      {{hoso.mahoso}}
+        <button type="button" class="m-0 p-0 btn btn-sm btn-success" @click="save_ttdl">
+          <i class="fa fa-save">{{stt + 1}}</i>
+        </button>
+        <button type="button" class="m-0 p-0 btn btn-sm btn-danger" @click="stop_ttdl">
+          <i class="fa fa-stop-circle">Bỏ</i>
+        </button>
+      </div>
+    </div>
+
+  </div>
+  <div v-else class="row m-0 p-0 bg-sucess">
+    <div class="col-1 crud text-break">
+    {{hoso.mahoso}}
+      <button type="button" class="m-0 p-0 btn btn-sm btn-info" @click="edit_ttdl">
+        <i class="fa fa-edit">{{stt + 1}}</i>
+      </button>
+    </div>
+  </div>
+  `,
+  data() {
+    return {
+      isedit: rec.isedit,
+      hoso: rec.hoso,
+      dot: rec.dot,
+      khachhang: rec.khachhang,
+    };
+  },
+  methods: {
+    stop_ttdl() {
+      this.isedit = false;
+      console.log('stop_ttdl');
+    },
+    edit_ttdl() {
+      this.isedit = true;
+      console.log('edit_ttdl');
+    },
+    save_ttdl() {
+      this.isedit = false;
+      console.log('save_ttdl');
+    },
+  },
+});
