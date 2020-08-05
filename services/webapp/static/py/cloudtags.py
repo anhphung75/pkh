54ca4295143fd6035cc07fc0660d556422ff6563
anhphung75 <anhphung75@gmail.com>
2020-08-05 17:33:06 +0700

    tiep 20200805

---
+++ services/webapp/static/py/cloudtags.py
@@ -0,0 +1,41 @@
+from browser import bin, document as tags
+from browser.html import DIV, BUTTON, I, SPAN
+
+container = tags["app"]
+stim = ''
+otim = {"2020": 0, "anh": 1}
+
+# cloudtags = showtags + inp
+
+def clear_otim(ev):
+    otim = {}
+
+
+def del_otim(k):
+    if k in otim:
+        del otim[k]
+
+
+def show_cloudtags(ev):
+    cloudtags = SPAN(id="cloudtags")
+    
+    button = BUTTON(
+        I(Class="fa fa-trash"),
+        id="clear_button",
+        type="button",
+        style=dict(color="red", paddingLeft="1em"),
+        Class="down",
+    )
+
+    @bind("#clear_button", "click")
+    def change(ev):
+        display = document["menu"].style.display
+        document["menu"].style.display = "block" if display == "none" else "none"
+        print(ev.target.text, ev.x, ev.y)
+    for k in otim:
+        cloudtags <= BUTTON(k,
+                            style=dict(color="red", paddingLeft="1em"),
+                            Class="down")
+
+    container.clear()
+    container <= cloudtags
--- services/webapp/ttdl/bangbieu.py
+++ services/webapp/ttdl/bangbieu.py
@@ -3,7 +3,7 @@ import sys
 import sqlite3
 import datetime
 import urllib
-from utils.thoigian import stodate, datetos
+#from utils.thoigian import stodate, datetos
 from sqlalchemy import Column, Sequence, func, desc
 from sqlalchemy import Boolean, Integer, DECIMAL, Unicode, Date, DateTime, VARBINARY
 from sqlalchemy import ForeignKey
@@ -14,42 +14,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-class Hoso(Base):
-    __tablename__ = 'hoso'
-#    __table_args__ = {"schema": "web"}
-    autoid = Column(Integer, primary_key=True)
-    mahoso = Column(Unicode(13), unique=True)  # yyyy.hs.xxxxxx
-    qrcode = Column(VARBINARY)
-    sodanhbo = Column(Unicode(50))  # barcode
-    madot = Column(Unicode(50))
-    sohoso = Column(Unicode(50))
-    khachhang = Column(Unicode(255))
-    diachi = Column(Unicode(255))
-    ghitat = Column(Unicode(50))
-    lienhe = Column(Unicode(50))
-    dc1 = Column(Unicode(50))
-    dc2 = Column(Unicode(50))
-    maq = Column(Unicode(2), default='--')
-    maqp = Column(Unicode(4), default='----')
-    mota = Column(Unicode(255))
-    # pkh
-    trongai = Column(Unicode(50))
-    tainhap = Column(Unicode(50))
-    taithicong = Column(Unicode(50))
-    hoantien = Column(Unicode(50))
-    # hoan cong
-    maqt = Column(Unicode(50))
-    ngaylendot = Column(DateTime(timezone=True))
-    ngaygan = Column(DateTime(timezone=True))
-    sodhn = Column(Unicode(50))
-    hieudhn = Column(Unicode(50))
-    # tong cong 19 so, co 5 so thap phan
-    chisodhn = Column(DECIMAL(19, 5), default=0)
-    madma = Column(Unicode(50))
-    malotrinh = Column(Unicode(50))
-    # ref old data
-    hosoid = Column(Integer)
-    id_new = Column(Integer)
-    id_old = Column(Integer)
+class DinhmucChiPhi(Base):
+    __tablename__ = 'dinhmucchiphi'
+    __table_args__ = {"schema": "dbo"}
+    madmcp = Column(Unicode(10), primary_key=True)  # yyyy.hs.xxxxxx
+    mota = Column(Unicode(None))
     lastupdate = Column(DateTime(timezone=True), default=func.now(),
                         onupdate=datetime.datetime.now)
