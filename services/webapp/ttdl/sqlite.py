883dad3feb4069a65b3b94c82e97063d9184f96f
anhphung75 <anhphung75@gmail.com>
2021-01-05 02:39:57 +0700

    tiep 20200105

--- services/webapp/ttdl/sqlite.py
+++ services/webapp/ttdl/sqlite.py
@@ -1,110 +1,85 @@
 import os
 import sys
-import datetime
 import decimal
 import arrow
+import json
 #from utils.thoigian import stodate, datetos
 from sqlalchemy import create_engine, ForeignKey, inspect
 from sqlalchemy import Column, Sequence, func, desc
 from sqlalchemy import Boolean, Integer, DECIMAL, Unicode, VARBINARY, JSON, Date, DateTime
-from sqlalchemy.dialects.postgresql import JSONB
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.mutable import MutableDict
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 
 
 Base = declarative_base()
 
 
 class Mau(object):
-    __table_args__ = {"schema": "web"}
-    idutc = Column(Integer, primary_key=True)
-    refs = Column(MutableDict.as_mutable(JSON))
-    data = Column(MutableDict.as_mutable(JSON))
-    #refs = Column(mutable_json_type(dbtype=JSON, nested=True))
-    #data = Column(mutable_json_type(dbtype=JSON, nested=True))
-    slang = Column(Unicode(255))
+    #__table_args__ = {"schema": "web"}
+    idutc = Column(Integer, primary_key=True, autoincrement=False)
+    _refs = Column(Unicode())
+    _data = Column(Unicode())
     status = Column(Unicode(50))
     inok = Column(Boolean, default=False)
     lastupdate = Column(Integer,
                         default=int(arrow.utcnow().float_timestamp * 1000),
                         onupdate=int(arrow.utcnow().float_timestamp * 1000))
 
+    @hybrid_property
+    def refs(self):
+        try:
+            dl = json.loads(self._refs)
+            print(f"hydrid property refs")
+            return dl
+        except:
+            return self._refs
 
-class Test_table_arg(Mau, Base):
-    __tablename__ = 'test_table_arg'
+    @refs.setter
+    def refs(self, new):
+        print(f"refs setter new={new}")
+        dl = {}
+        try:
+            dl = json.loads(self._refs)
+        except:
+            pass
+        for k in new:
+            if (k in ['ghichu', 'notes']) or (new[k] and (f'new[k]' not in ['', '{}', '[]'])):
+                dl[k] = new[k]
+        if len(dl) > 0:
+            self._refs = json.dumps(dl, ensure_ascii=False)
 
+    @hybrid_property
+    def data(self):
+        try:
+            dl = json.loads(self._data)
+        except:
+            dl = {}
+        return dl
 
-class Khachhang(Mau, Base):
-    __tablename__ = 'khachhang'
-    __table_args__ = {"schema": "web"}
-    makhachhang = Column(Unicode(50))  # yyyy.kh.xxxxxx xx:stt
-    # lkn1_hoso_khachhang = relationship(
-    #    "Hoso", back_populates="lk1n_khachhang_hoso")
-
-
-class Hoso(Mau, Base):
-    __tablename__ = 'hoso'
-    __table_args__ = {"schema": "web"}
-    mahoso = Column(Unicode(50))  # yyyy.hs.xxxxxx
-    # lk1n_khachhang_hoso = relationship(
-    #    "Khachhang", back_populates="lkn1_hoso_khachhang")
-
-
-class Khuvuc(Mau, Base):
-    __tablename__ = 'khuvuc'
-    __table_args__ = {"schema": "web"}
-    makhuvuc = Column(Unicode(50))  # yyyy.hs.xxxxxx
-
-
-class Dot(Mau, Base):
-    __tablename__ = 'dot'
-    __table_args__ = {"schema": "web"}
-    madot = Column(Unicode(50))  # yyyy.gmmp.xxx xxx:stt
-    # refs:= dvtc.idutc, madvtc, qtvt.idutc, maqtvt
-    # data:= qtgt:tonghoso:, tongqt, tongtrongai,...,nguoilap, ngaylap,ghichu; qtvt:sophieunhap,
-
-
-class Donvithicong(Mau, Base):
-    __tablename__ = 'donvithicong'
-    __table_args__ = {"schema": "web"}
-    madvtc = Column(Unicode(50))
-    # data:= lienhe, masothue, ....
+    @data.setter
+    def data(self, new):
+        dl = {}
+        try:
+            dl = json.loads(self._data)
+        except:
+            pass
+        for k in new:
+            if (k in ['ghichu', 'notes']) or (new[k] and (f'new[k]' not in ['', '{}', '[]'])):
+                dl[k] = new[k]
+        print("data setter new={new}")
+        if len(dl) > 0:
+            self._data = json.dumps(dl, ensure_ascii=False)
 
 
 class ChiphiQuanly(Mau, Base):
     __tablename__ = 'chiphiquanly'
-    __table_args__ = {"schema": "web"}
-    macpql = Column(Unicode(50))  # yyyy.hs.xxxxxx
-
-
-class Bgvl(Mau, Base):
-    __tablename__ = 'bgvl'
-    __table_args__ = {"schema": "web"}
-
-
-class Bgnc(Mau, Base):
-    __tablename__ = 'bgnc'
-    __table_args__ = {"schema": "web"}
-
-
-class Bgmtc(Mau, Base):
-    __tablename__ = 'bgmtc'
-    __table_args__ = {"schema": "web"}
-
-
-class Bgtl(Mau, Base):
-    __tablename__ = 'bgtl'
-    __table_args__ = {"schema": "web"}
 
 
 class Server():
-    def __init__(self, user=None, pwd=None, host=None, dbname="draft.db"):
-        self.user = user
-        self.pwd = pwd
-        self.host = host
-        self.dbname = dbname
-        self.cnnstr = f"sqlite:///{self.dbname}"
+    def __init__(self, dbname="draft.db"):
+        self.cnnstr = f"sqlite:///{dbname}"
 
     def core(self):
         try:
@@ -119,12 +94,13 @@ class Server():
         return engine
 
     def orm(self):
-        try:
-            engine = create_engine(self.cnnstr, echo=True)
-            Base.metadata.create_all(engine)
-            Session = scoped_session(sessionmaker(bind=engine, autoflush=True))
-        except:
-            return None
+        # try:
+        engine = create_engine(self.cnnstr, echo=True)
+        Base.metadata.create_all(engine)
+        Session = scoped_session(sessionmaker(bind=engine, autoflush=True))
+        # except:
+        #    print("err orm")
+        #    return None
         return Session()
 
     def show_views(self, schema=None):
@@ -148,31 +124,212 @@ class Server():
             pass
 
 
-def runsql(sql=''):
-    engine = Server()
-    try:
-        kq = engine.core().execute(sql)
-        data = []
-        for row in kq:
-            dl = dict(row)
+class Orm():
+    def __init__(self, schema='web'):
+        self.schema = schema.lower()
+        self.bdl = None
+        self.server()
+
+    def server(self):
+        engine = Server('draft.db')
+        self.orm = engine.orm()
+
+    def thongtin(self, bang):
+        try:
+            bang = bang.lower()
+            if bang in ['chiphiquanly', 'cpql']:
+                self.bdl = ChiphiQuanly
+            else:
+                self.bdl = None
+        except:
+            pass
+
+    def gom(self, bang, stim):
+        self.thongtin(bang)
+        if self.bdl == None:
+            return None
+        try:
+            stim = f"{stim}".lower()
+            r = self.orm.query(self.bdl).filter(
+                (self.bdl._refs.like('%stim%')) | (self.bdl._data.like('%stim%'))).all()
+            print(f"orm cpql r = {r}")
+            return r
+        except:
+            return None
+
+    def nap(self, bang, idutc=0):
+        self.thongtin(bang)
+        # try:
+        idutc = int(idutc)
+        r = self.orm.query(self.bdl).filter(
+            self.bdl.idutc == idutc).first()
+        print(f"orm nap r = {vars(r)}")
+        return r
+        # except:
+        #    return None
+
+    def moi(self, bang, dl):
+        self.thongtin(bang)
+        if (self.bdl == None) or ('idutc' not in dl):
+            return None
+        if dl and ('refs' not in dl) and ('data' not in dl):
+            return None
+        s = self.bdl()
+        if "refs" in dl:
+            s.refs = dl['refs']
+        if "data" in dl:
+            s.data = dl['data']
+        if 'status' in dl:
+            s.status = dl['status']
+        if 'inok' in dl:
+            s.inok = dl['inok']
+        while True:
+            s.idutc = int(dl['idutc'])
+            s.lastupdate = int(arrow.utcnow().float_timestamp * 1000)
+            try:
+                print(f'orm moi refs={s.refs} _refs={s._refs}')
+                print(f'orm moi data={s.data} _data={s._data}')
+                self.orm.add(s)
+                self.orm.commit()
+                break
+            except IntegrityError as err:
+                try:
+                    info = f"{err}"
+                    if ('UNIQUE' in info) or ('PRIMARY' in info):
+                        # duplicate Primary key
+                        self.orm.rollback()
+                        dl['idutc'] += 1
+                except IntegrityError as err:
+                    break
+
+    def read(self, bang, idutc):
+        self.nap(bang, idutc)
+
+    def save1(self, bang='', dl={}):
+        self.thongtin(bang)
+        if self.bdl == None:
+            return None
+        if dl and (('refs' in dl) or ('data' in dl)):
             for k in dl.copy():
-                if type(dl[k]) in [datetime, datetime.date, datetime.datetime, datetime.time]:
-                    if k == 'lastupdate':
-                        dl[k] = int(arrow.get(dl[k]).float_timestamp * 1000)
+                if k not in ['idutc', 'inok', 'lastupdate', 'status', 'refs', 'data']:
+                    del dl[k]
+        else:
+            return None
+        # check dl
+        if 'data' in dl:
+            if isinstance(dl['data'], str):
+                dl['data'] = json.loads(dl['data'])
+            try:
+                for k in dl['data'].copy():
+                    if (dl['data'][k] == None) or (len(dl['data'][k]) < 1):
+                        if k.lower() not in ['ghichu', 'notes']:
+                            del dl['data'][k]
+                    for k1 in k:
+                        if (dl['data'][k][k1] == None) or (len(dl['data'][k][k1]) < 1):
+                            if k1.lower() not in ['ghichu', 'notes']:
+                                del dl['data'][k][k1]
+            except:
+                pass
+        if 'refs' in dl:
+            if isinstance(dl['refs'], str):
+                dl["refs"] = json.loads(dl["refs"])
+            try:
+                for k in dl['refs'].copy():
+                    if (dl['refs'][k] == None) or (len(dl['refs'][k]) < 1):
+                        del dl['refs'][k]
                     else:
-                        dl[k] = int(arrow.get(dl[k]).format("YYYYMMDD"))
-                if isinstance(dl[k], decimal.Decimal):
-                    dl[k] = float(dl[k])
-            data.append(dl)
-        kq.close()
-        for cp in data:
-            print(f"runsql cp={cp}")
-        return data
-    except:
-        return None
+                        for k1 in k:
+                            if (dl['refs'][k][k1] == None) or (len(dl['refs'][k][k1]) < 1):
+                                del dl['refs'][k][k1]
+            except:
+                pass
+        if "idutc" not in dl:
+            dl['idutc'] = int(arrow.utcnow().float_timestamp * 1000)
+            self.moi(bang, dl)
+            return None
+        s = self.nap(bang, dl['idutc'])
+        if s == None:
+            self.moi(bang, dl)
+            return None
+        if "refs" in dl:
+            s.refs = dl['refs']
+        if "data" in dl:
+            s.data = dl['data']
+        if 'status' in dl:
+            s.status = dl['status']
+        if 'inok' in dl:
+            s.inok = dl['inok']
+        s.idutc = int(dl['idutc'])
+        s.lastupdate = int(arrow.utcnow().float_timestamp * 1000)
+        try:
+            self.orm.commit()
+        except IntegrityError as err:
+            try:
+                print(f"save1 err={err.orig.args[1]}")
+            except:
+                return None
 
 
-engine = Server("pkh", "Ph0ngK3H0@ch",
-                "192.168.24.4:1433", "PKHData")
-engine.orm()
-engine.show_views('web')
+class TaoJson():
+    def __init__(self, schema='web'):
+        self.schema = schema
+        self.chiphiquanly()
+
+    def khuvuc(self):
+        pass
+
+    def chiphiquanly(self):
+        dl = {
+            "idutc": int(arrow.get("20200101").to('utc').float_timestamp * 1000),
+            "refs": {"macpql": 1},
+            "data": {"vl": 1, "nc": 2.289, "mtc": 1.26, "tructiepkhac": 0.015, "chung": 0.045, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053},
+            "status": "Fin"
+        }
+        Orm("web").moi("chiphiquanly", dl)
+
+    def chiphiquanly_old(self):
+        dl = {"macpql": 1, "status": "Fin",
+              "data": {"vl": 1, "nc": 2.289, "mtc": 1.26, "tructiepkhac": 0.015, "chung": 0.045, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053}
+              }
+        Rest("web", "chiphiquanly").sua(dl)
+        dl = {"macpql": 2, "status": "Fin",
+              "data": {"vl": 1, "nc": 2.8, "mtc": 1.34, "tructiepkhac": 0.02, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053}
+              }
+        Rest("web", "chiphiquanly").save(dl)
+
+        dl = {"macpql": 3, "status": "Fin",
+              "data": {"vl": 1, "nc": 2.289, "mtc": 1.26, "tructiepkhac": 0.02, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 4, "status": "Fin",
+              "data": {"vl": 1, "nc": 3.857, "mtc": 1.504, "tructiepkhac": 0.02, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 5, "status": "Fin",
+              "data": {"vl": 1, "nc": 5.714, "mtc": 1.82, "tructiepkhac": 0.02, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053,
+                       "phaply": "Nghị định 32/2015/NĐ-CP ngày 25/03/2015; Quyết định 3384/QĐ-UBND 02/07/2016"}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 6, "status": "Fin",
+              "data": {"vl": 1, "nc": 1, "mtc": 1, "tructiepkhac": 0, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.3, "giamsat": 0.02053,
+                       "phaply": "Nghị định 32/2015/NĐ-CP ngày 25/03/2015; Quyết định 3384/QĐ-UBND 02/07/2016"}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 7, "status": "Fin",
+              "data": {"vl": 1, "nc": 1, "mtc": 1, "tructiepkhac": 0, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0236, "thietke": 1.2, "giamsat": 0.02566,
+                       "phaply": "Nghị định 32/2015/NĐ-CP ngày 25/03/2015; Quyết định 3384/QĐ-UBND 02/07/2016"}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 20190725, "status": "Fin",
+              "data": {"vl": 1, "nc": 1, "mtc": 1, "tructiepkhac": 0, "chung": 0.05, "giantiepkhac": 0, "thutinhtruoc": 0.055, "khaosat": 0.0236, "thietke": 1.2, "giamsat": 0.02566,
+                       "phaply": "Nghị định 68/2019/NĐ-CP ngày 14/08/2019; Quyết định 2207/QĐ-UBND ngày 18/06/2020"}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+        dl = {"macpql": 20200721, "status": "Fin",
+              "data": {"vl": 1, "nc": 1, "mtc": 1, "tructiepkhac": 0, "chung": 0.055, "giantiepkhac": 0.02, "thutinhtruoc": 0.055, "khaosat": 0.0207, "thietke": 1.2, "giamsat": 0.02566,
+                       "phaply": "Nghị định 68/2019/NĐ-CP ngày 14/08/2019; Quyết định 2207/QĐ-UBND ngày 18/06/2020"}
+              }
+        Rest("web", "chiphiquanly").moi(dl)
+
+
+TaoJson()
--- services/webapp/ttdl/tets.py
+++ services/webapp/ttdl/tets.py
@@ -1,198 +1,86 @@
 import os
 import sys
-import datetime
 import decimal
 import arrow
 import json
-#from utils.thoigian import stodate, datetos
+
 from sqlalchemy import create_engine, ForeignKey, inspect
 from sqlalchemy import Column, Sequence, func, desc
-from sqlalchemy import Integer, Unicode, JSON, Boolean,  DECIMAL,  VARBINARY,  Date, DateTime
-from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy import Boolean, Integer, DECIMAL, Unicode, VARBINARY, JSON, Date, DateTime
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.ext.mutable import MutableDict
-from sqlalchemy_json import mutable_json_type
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
 
 
 Base = declarative_base()
 
 
-class Mau(object):
-    __table_args__ = {"schema": "web"}
-    idutc = Column(Integer, primary_key=True)
-    refs = Column(Unicode())
-    data = Column(Unicode())
-    #refs = Column(MutableDict.as_mutable(JSON))
-    #data = Column(MutableDict.as_mutable(JSON))
-    #refs = Column(mutable_json_type(dbtype=JSON, nested=True))
-    #data = Column(mutable_json_type(dbtype=JSON, nested=True))
-    status = Column(Unicode(50))
-    inok = Column(Boolean, default=False)
-    lastupdate = Column(Integer,
-                        default=int(arrow.utcnow().float_timestamp * 1000),
-                        onupdate=int(arrow.utcnow().float_timestamp * 1000))
+class Serializable(object):
+    __public__ = []
+
+    def to_dict(self):
+        d = {}
+        for field in self.__public__:
+            value = getattr(self, field)
+            if value:
+                d[field] = value
+        return d
 
 
-class Test_table_arg(Mau, Base):
-    __tablename__ = 'test_table_arg'
+class Example(Base):
+    __tablename__ = 'example'
+    __public__ = ['column1', 'refs']
 
+    id = Column(Integer, primary_key=True)
+    column1 = Column(Unicode(32))
+    _refs = Column(Unicode())
 
-class Khachhang(Mau, Base):
-    __tablename__ = 'khachhang'
-    makhachhang = Column(Unicode(50))  # yyyy.kh.xxxxxx xx:stt
-    # lkn1_hoso_khachhang = relationship(
-    #    "Hoso", back_populates="lk1n_khachhang_hoso")
-
-
-class Hoso(Mau, Base):
-    __tablename__ = 'hoso'
-    mahoso = Column(Unicode(50))  # yyyy.hs.xxxxxx
-    # lk1n_khachhang_hoso = relationship(
-    #    "Khachhang", back_populates="lkn1_hoso_khachhang")
-
-
-class Khuvuc(Mau, Base):
-    __tablename__ = 'khuvuc'
-    makhuvuc = Column(Unicode(50))  # yyyy.hs.xxxxxx
-
-
-class Dot(Mau, Base):
-    __tablename__ = 'dot'
-    madot = Column(Unicode(50))  # yyyy.gmmp.xxx xxx:stt
-    # refs:= dvtc.idutc, madvtc, qtvt.idutc, maqtvt
-    # data:= qtgt:tonghoso:, tongqt, tongtrongai,...,nguoilap, ngaylap,ghichu; qtvt:sophieunhap,
-
-
-class Donvithicong(Mau, Base):
-    __tablename__ = 'donvithicong'
-    madvtc = Column(Unicode(50))
-    # data:= lienhe, masothue, ....
-
-
-class ChiphiQuanly(Mau, Base):
-    __tablename__ = 'chiphiquanly'
-    macpql = Column(Unicode(50))  # yyyy.hs.xxxxxx
-
-
-class Bgvl(Mau, Base):
-    __tablename__ = 'bgvl'
-
-
-class Bgnc(Mau, Base):
-    __tablename__ = 'bgnc'
-
-
-class Bgmtc(Mau, Base):
-    __tablename__ = 'bgmtc'
-
-
-class Bgtl(Mau, Base):
-    __tablename__ = 'bgtl'
-
-
-class Server():
-    def __init__(self, user=None, pwd=None, host=None, dbname=None):
-        self.user = user
-        self.pwd = pwd
-        self.host = host
-        self.dbname = dbname
-        self.cnnstr = (
-            f"mssql+pyodbc://{self.user}:{self.pwd}@{self.host}/{self.dbname}?"
-            f"driver=ODBC+Driver+17+for+SQL+Server")
-
-    def core(self):
+    @hybrid_property
+    def refs(self):
         try:
-            # params = urllib.parse.quote_plus(
-            #    "DRIVER={FreeTDS};SERVER=mssql;Port:1433;DATABASE=master;UID=sa;PWD=w3b@pkh2019")
-            # cnnstr = f"mssql+pyodbc:///?odbc_connect={params}"
-            #            cnnstr = f"sqlite:///:memory:"
-            engine = create_engine(self.cnnstr, echo=True)
-            engine.execution_options(isolation_level="AUTOCOMMIT")
+            value = json.loads(self._refs)
         except:
-            return None
-        return engine
+            value = {}
+        return value
 
-    def orm(self):
-        # try:
-        #cnnstr = f"sqlite:///:memory:"
-        engine = create_engine(self.cnnstr, echo=True)
-        Base.metadata.create_all(engine)
-        Session = scoped_session(sessionmaker(bind=engine, autoflush=True))
-        # except:
-        # return None
-        return Session()
-
-    def show_views(self, schema=None):
-        sql = f"Select * From Information_schema.tables"
-        if schema:
-            sql += f" Where table_schema='{schema}'"
-        sql += f" Order by table_catalog, table_schema, table_type, table_name;"
+    @refs.setter
+    def refs(self, new):
+        dl = {}
         try:
-            kq = self.core().execute(sql)
-            dl = []
-            for row in kq:
-                a = dict(row)
-                dl.append(a)
-                print(a)
-            return dl
+            dl = json.loads(self._refs)
         except:
             pass
-
-    def del_object(self, otype='Table', oname=None):
-        sql = (
-            f"Drop {otype} {oname};")
-        try:
-            self.core().execute(sql)
-        except:
-            pass
-
-
-class DoiJson():
-    def __init__(self, schema='dbo'):
-        self.schema = schema
-        self.svread = null
-        self.svsave = null
-    
-    def open_svread(self):
-        return engine
-    
-    def khachhang(self):
-        # load
-        sql = (
-            f"Select top 1 sodot, ngaylap, khuvuc,isnull(nhathauid,0) as dvtcid,"
-            f" isnull(dautucty,0) as tiencty,isnull(dautukhach,0) as tienkhach"
-            f" From {self.schema}.dot"
-            f" Where (madot='{self.madot}')"
-        )
-        dl = runsql(sql)
-        if ((dl == None) or (len(dl) < 1)):
-            return
+        print(f"refs setter new={new}")
+        for k in new:
+            if new[k]!=None or len(new[k]) > 0:
+                dl[k] = new[k]
+        
+        if len(dl) > 0:
+            self._refs = json.dumps(dl, ensure_ascii=False)
+        
 
 
-def runsql(sql=''):
-    engine = Server("pkh", "Ph0ngK3H0@ch",
-                    "192.168.24.4:1433", "PKHData")
-    try:
-        kq = engine.core().execute(sql)
-        data = []
-        for row in kq:
-            dl = dict(row)
-            for k in dl.copy():
-                if type(dl[k]) in [datetime, datetime.date, datetime.datetime, datetime.time]:
-                    if k == 'lastupdate':
-                        dl[k] = int(arrow.get(dl[k]).to(
-                            'utc').float_timestamp * 1000)
-                    else:
-                        dl[k] = int(arrow.get(dl[k]).format("YYYYMMDD"))
-                if isinstance(dl[k], decimal.Decimal):
-                    dl[k] = float(dl[k])
-            data.append(dl)
-        kq.close()
-        return data
-    except:
-        return None
+def create_db_session():
+    connect_string = 'sqlite:///:memory:'
+    engine = create_engine(connect_string)
+    Base.metadata.create_all(bind=engine)
+    return sessionmaker(bind=engine)()
 
 
-engine = Server("pkh", "Ph0ngK3H0@ch",
-                "192.168.24.4:1433", "PKHData")
-dl = engine.show_views("web")
+def main():
+    session = create_db_session()
+
+    example = Example()
+    example.status = "String content"
+    example.refs = {'foo': 42}
+    session.add(example)
+    session.commit()
+
+    obj = session.query(Example).first()
+    print(f"kq={vars(obj)}")
+    print(f"data={obj.refs}")
+
+
+if __name__ == '__main__':
+    main()
