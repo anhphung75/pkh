5e32f0838e48703c63a51c630093b89dd12e5e70
anhphung75 <anhphung75@gmail.com>
2021-03-23 00:16:16 +0700

    tiep

--- services/webapp/static/js/forms/qtgt/ct.js
+++ services/webapp/static/js/forms/qtgt/ct.js
@@ -1333,6 +1333,91 @@ const web = {
       .text([cv, "%"].join(''));
     if (cv === 100) { setTimeout(zone.remove(), 300); }
   },
+  renderProgress:(el)=> {
+    var start = 0;
+    var end = el.dataset.progress;
+    
+    var colours = {
+      fill: '#' + el.dataset.fillColour,
+      track: '#' + el.dataset.trackColour,
+      text: '#' + el.dataset.textColour,
+      stroke: '#' + el.dataset.strokeColour,
+    }
+    
+    var radius = 100;
+    var border = el.dataset.trackWidth;
+    var strokeSpacing = el.dataset.strokeSpacing;
+    var endAngle = Math.PI * 2;
+    var formatText = d3.format('.0%');
+    var boxSize = radius * 2;
+    var count = end;
+    var progress = start;
+    var step = end < start ? -0.01 : 0.01;
+    
+    //Define the circle
+    var circle = d3.arc()
+      .startAngle(0)
+      .innerRadius(radius)
+      .outerRadius(radius - border);
+    
+    //setup SVG wrapper
+    var svg = d3.select(el)
+      .append('svg')
+      .attr('width', boxSize)
+      .attr('height', boxSize);
+    
+    // ADD Group container
+    var g = svg.append('g')
+      .attr('transform', 'translate(' + boxSize / 2 + ',' + boxSize / 2 + ')');
+    
+    //Setup track
+    var track = g.append('g').attr('class', 'radial-progress');
+    track.append('path')
+      .attr('class', 'radial-progress__background')
+      .attr('fill', colours.track)
+      .attr('stroke', colours.stroke)
+      .attr('stroke-width', strokeSpacing + 'px')
+      .attr('d', circle.endAngle(endAngle));
+    
+    //Add colour fill
+    var value = track.append('path')
+      .attr('class', 'radial-progress__value')
+      .attr('fill', colours.fill)
+      .attr('stroke', colours.stroke)
+      .attr('stroke-width', strokeSpacing + 'px');
+    
+    //Add text value
+    var numberText = track.append('text')
+      .attr('class', 'radial-progress__text')
+      .attr('fill', colours.text)
+      .attr('font-size', '30px')
+      .attr('text-anchor', 'middle')
+      .attr('dy', '.5rem');
+    
+    function update(progress) {
+      //update position of endAngle
+      value.attr('d', circle.endAngle(endAngle * progress));
+      //update text value
+      numberText.text(formatText(progress));
+    };
+    
+    function iterate() {
+      //call update to begin animation
+      update(progress);
+      
+      if (count > 0) {
+        //reduce count till it reaches 0
+        count--;
+        //increase progress
+        progress += step;
+        //Control the speed of the fill
+        setTimeout(iterate, 10);
+      }
+    };
+    
+    iterate();
+  },
+
   hov_intag: (tagid = null) => {
     let tag, zone, row, col,
       hov = "yellow",
